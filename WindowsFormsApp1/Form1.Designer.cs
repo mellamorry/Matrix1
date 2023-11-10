6c794d37468d13c892a5ea7e7539ba3b6eeb9619
mellamorry <149560372+mellamorry@users.noreply.github.com>
2023-11-10 14:21:24 +0700

    Add program
    Add program frame

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Заполнить матрицу случайными числами";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заполнить матрицу с формы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 226);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 421);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static int dim = 3; //размерность (dimention)
+        int[,] OrigMatrix = new int [dim,dim]; //исходная матрица (original matrix)
+        int i, j;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //При загрузке формы настраивается оформление: 3 строки и 3 столбца, заголовки строк и столбцов не видно
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();     
+            dataGridView1.ColumnCount = dim;
+            dataGridView1.RowCount= dim;
+            for (i = 0; i < dim; i++)
+            {
+                dataGridView1.Columns[i].Width = Convert.ToInt32(dataGridView1.Width / dim-1);
+            }
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;   
+        }
+        
+        //Обработка значений, введённых в dataGridView1: если в ячейку введено что-то отличное от целого числа, то
+        //в ячейку записывается 0. Событие - изменение значения в ячейке
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //пытаемся выполнить
+            try 
+                {
+                //e.RowIndex и e.ColumnIndex - это индексы той ячейки, на которую сработало событие (которая была изменена)
+                int MatrixValue = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                }
+            //что делать, если не получилось выполнить блок try
+            catch 
+                {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 0;
+                listBox1.Items.Add($"В матрице элемент {e.RowIndex+1}x{e.ColumnIndex+1} был заменён на 0.");
+                }
+        }
+
+        //Заполнение матрицы значениями, введенными в dataGridView1
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < dim; i++)
+            {
+                for (j = 0; j < dim; j++)
+                {
+                    OrigMatrix[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                }
+            }
+        }
+
+        //Заполнение матрицы случайными значениями и вывод в dataGridView1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (i = 0; i < dim; i++)
+                {
+                for (j=0; j < dim; j++ )
+                    {
+                    OrigMatrix[i, j] = rnd.Next(0, 10);
+                    dataGridView1.Rows[i].Cells[j].Value = OrigMatrix[i, j];
+                    }
+                }
+            
+        }
+
+    }
+}
